a172ea7fccd3733f6689381b3468c3cc7d4d7f73
qqchrono <hazification@gmail.com>
2023-06-08 22:21:06 +0800

    working on tickets and technical staff navbar

---
+++ Website Administrator with CSS/Tickets/Complaint Tickets/complaintTicketHomepage.php
@@ -0,0 +1,49 @@
+<?php
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Water Supply Management</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="ticketHomepage.css">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+</head>
+<body>
+	<?php include 'ticketHomepageNavbar.html';?>
+	
+    <h3 class="heading-gap">Current Tickets</h3>
+    
+    <!-- Table of tickets go here -->
+		<table>
+		<tr>
+		  <th>Ticket ID</th>
+		  <th>Customer Name</th>
+		  <th>Status</th>
+		  <th>Details</th>
+		  <th>Time of Issue</th>
+		  <th>Type</th>
+		  <th colspan="2"></th>
+		</tr>
+		<tr>
+		  <td>Data</td>
+		  <td>Data</td>
+		  <td>Data</td>
+		  <td>Data</td>
+		  <td>Data</td>
+		  <td>Data</td>
+		  <td><button>Assign</button></td>
+		  <td><a href="#"><button class="arrow-button"></button></td>
+		</tr>
+	</table>
+
+</body>
+</html>
