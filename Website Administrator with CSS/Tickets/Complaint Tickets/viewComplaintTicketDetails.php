902dbf762966653e9022c09536d0b70782515b2c
pinkylhx <pinky3301@gmail.com>
2023-06-14 01:33:52 +0800

    view complaint ticket css

--- Website Administrator with CSS/Tickets/Complaint Tickets/viewComplaintTicketDetails.php
+++ Website Administrator with CSS/Tickets/Complaint Tickets/viewComplaintTicketDetails.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../ticketHomepage.css">
+    <link rel="stylesheet" href="viewComplaintTicketDetails.css">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </head>
@@ -82,42 +82,38 @@
         {
         ?>
             <form action="assignTicketForm.php" method="POST">
-                <table>
-                    <tr>
-                        <td><label for="complaintTicketID">Ticket ID : </label>
-                        <input type='text' id="complaintTicketID" name='complaintTicketID' value='<?php echo $complaintTicketID ?>' readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="customerName">Customer Name : </label>
-                        <input type="text" id="customerName" value="<?php echo $customerName ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="staffName">Staff Name : </label>
-                        <input type="text" id="staffName" value="<?php echo $staffName ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="ticketStatus">Ticket Status : </label>
-                        <input type="text" id="ticketStatus" value="<?php echo $ticketStatus ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="details">Details : </label>
-                        <input type="text" id="details" value="<?php echo $details ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="time_of_issue">Time of issue : </label>
-                        <input type="text" id="time_of_issue" value="<?php echo $timeOfIssue ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="time_of_resolution">Time of resolution : </label>
-                        <input type="text" id="time_of_resolution" value="<?php echo $timeOfResolution ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td class="button-container">
-                            <input type="submit" name="assignTicket" value="Assign Ticket" style="border-radius: 5px;">
-                            <a href="complaintTicketHomepage.php"><button type="button" style="border-radius: 5px">Back</button></a>
-                        </td>
-                    </tr>
-                </table>
+                <div class="form-row">
+                    <label for="complaintTicketID">Ticket ID : </label>
+                    <div class="form-value"><?php echo $complaintTicketID ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="customerName">Customer Name : </label>
+                    <div class="form-value"><?php echo $customerName ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="staffName">Staff Name : </label>
+                    <div class="form-value"><?php echo $staffName ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="ticketStatus">Ticket Status : </label>
+                    <div class="form-value"><?php echo $ticketStatus ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="details">Details : </label>
+                    <div class="form-value"><?php echo $details ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="time_of_issue">Time of issue : </label>
+                    <div class="form-value"><?php echo $timeOfIssue ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="time_of_resolution">Time of resolution : </label>
+                    <div class="form-value"><?php echo $timeOfResolution ?></div>
+                </div>
+                <div class="button-container">
+                   <input type="submit" name="assignTicket" value="Assign Ticket" style="border-radius: 5px;">
+                    <a href="complaintTicketHomepage.php"><button type="button" style="border-radius: 5px">Back</button></a>
+                </div>
             </form>
     <?php
         }
@@ -125,43 +121,39 @@
         {
         ?>
             <form action="viewComplaintTicketDetails.php" method="POST">
-                <table>
-                    <tr>
-                        <td><label for="complaintTicketID">Ticket ID : </label>
-                        <input type='text' id="complaintTicketID" name='complaintTicketID' value='<?php echo $complaintTicketID ?>' readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="customerName">Customer Name : </label>
-                        <input type="text" id="customerName" value="<?php echo $customerName ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="staffName">Staff Name : </label>
-                        <input type="text" id="staffName" value="<?php echo $staffName ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="ticketStatus">Ticket Status : </label>
-                        <input type="text" id="ticketStatus" value="<?php echo $ticketStatus ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="details">Details : </label>
-                        <input type="text" id="details" value="<?php echo $details ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="time_of_issue">Time of issue : </label>
-                        <input type="text" id="time_of_issue" value="<?php echo $timeOfIssue ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td><label for="time_of_resolution">Time of resolution : </label>
-                        <input type="text" id="time_of_resolution" value="<?php echo $timeOfResolution ?>" readonly></td>
-                    </tr>
-                    <tr>
-                        <td class="button-container">
-                            <input type='hidden' name='staffID' value='<?php echo $hiddenStaffID ?>'>
-                            <input type="submit" name="resolveTicket" value="Resolve Ticket" style="border-radius: 5px;">
-                            <a href="complaintTicketHomepage.php"><button type="button" style="border-radius: 5px">Back</button></a>
-                        </td>
-                    </tr>
-                </table>
+            <div class="form-row">
+                    <label for="complaintTicketID">Ticket ID : </label>
+                    <div class="form-value"><?php echo $complaintTicketID ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="customerName">Customer Name : </label>
+                    <div class="form-value"><?php echo $customerName ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="staffName">Staff Name : </label>
+                    <div class="form-value"><?php echo $staffName ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="ticketStatus">Ticket Status : </label>
+                    <div class="form-value"><?php echo $ticketStatus ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="details">Details : </label>
+                    <div class="form-value"><?php echo $details ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="time_of_issue">Time of issue : </label>
+                    <div class="form-value"><?php echo $timeOfIssue ?></div>
+                </div>
+                <div class="form-row">
+                    <label for="time_of_resolution">Time of resolution : </label>
+                    <div class="form-value"><?php echo $timeOfResolution ?></div>
+                </div>
+                    <div class="button-container">
+                        <input type='hidden' name='staffID' value='<?php echo $hiddenStaffID ?>'>
+                        <input type="submit" name="resolveTicket" value="Resolve Ticket" style="border-radius: 5px;">
+                        <a href="complaintTicketHomepage.php"><button type="button" style="border-radius: 5px">Back</button></a>
+                    </div>
             </form>
         <?php
         }
