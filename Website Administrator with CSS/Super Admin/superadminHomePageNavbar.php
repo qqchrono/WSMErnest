400e23622e3c81915fcb3660f52f4d23de87fe19
qqchrono <hazification@gmail.com>
2023-07-05 15:48:03 +0800

    logout navbar fixed for superadmin

--- Website Administrator with CSS/Super Admin/superadminHomePageNavbar.php
+++ Website Administrator with CSS/Super Admin/superadminHomePageNavbar.php
@@ -1,7 +1,27 @@
+<?php
+	$staffID = $_SESSION['staffID'] ?? null;
+
+	if (isset($_POST['logout'])) {
+		session_unset();
+		session_destroy();
+		header("Location: ../LoginBoundary.php");
+		exit();
+	}
+?>
+
 <div class="w3-bar w3-blue w3-card">
 	<a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
 	<div class="navbar-links" style="justify-content: space-between;">
 			<a href="#" class="w3-bar-item w3-button w3-padding-large active">Super Admin</a>
-            <a href="#" class="w3-bar-item w3-button w3-padding-large" style="margin-left: 1120px";>Logout</a>
+			<div class="w3-dropdown-hover w3-hide-small">
+			<img src="../Account setting/upload/<?php echo $img_name; ?>" width="40" height="40" title="<?php echo $img_name; ?>" style="border-radius: 50%; cursor:pointer; margin-left:6px;">
+			<button class="w3-padding-large w3-button" title="More" style="width:1px;"><i class="fa">&#8942;</i></button>
+			<div class="w3-dropdown-content w3-bar-block w3-card-4" style="right:1px;">
+				<a href="../Account setting/AccountSetting.php" class="w3-bar-item w3-button">Account settings</a>
+				<form method="post" action="">
+					<button type="submit" name="logout" class="w3-bar-item w3-button" style="width:100%;">Log out</button>
+				</form>
+			</div>
+		</div>
 	</div>
 </div>
